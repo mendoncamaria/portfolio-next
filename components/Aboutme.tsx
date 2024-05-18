c5ec55984c0d8ea9f71807df4242d61ffb1e2f6f
Maria Mendonca <maria.mendonca156@gmail.com>
2024-05-18 14:40:24 +0530

    updated about section

--- components/Aboutme.tsx
+++ components/Aboutme.tsx
@@ -1,25 +1,24 @@
-import { SiInformatica } from "react-icons/si";
-import Title from "./Title";
+import { SiInformatica } from 'react-icons/si';
+import Title from './Title';
 
 const Aboutme = () => {
   return (
     <div className="wrapper">
       <Title text="About Me" icon={<SiInformatica />} />
       <div className="text-lg tracking-wide flex flex-col gap-6">
-      <p>
-          Welcome to my portfolio! I am an aspiring web developer with 1 year of
-          experience in building web applications. During my journey, I had the
-          opportunity to work on a banking website, where I gained valuable
-          insights into the industry and honed my skills in creating
-          user-friendly interfaces. Although I have primarily focused on a
-          single project, I have also invested time in personal projects to
-          expand my knowledge and explore different aspects of web development.
+        <p>
+          I&apos;m a passionate web developer with 2 years of experience
+          building user-friendly and dynamic web applications. My journey began
+          by contributing to a banking website, which provided valuable insights
+          into the industry and solidified my foundation in crafting intuitive
+          interfaces. While I honed my skills on this large-scale project,
+          I&apos;ve also actively pursued personal endeavors to explore various
+          facets of web development and stay ahead of the curve.
           <br />
-          One of my key areas of expertise is ReactJS. I thoroughly enjoy
-          leveraging its capabilities to create dynamic and responsive web
-          applications. I am always eager to learn and grow, and I am open to
-          collaborating with like-minded individuals and teams to take on new
-          and exciting challenges in the field of web development.
+          One of my key areas of expertise is ReactJS. I&apos;m a lifelong learner
+          with a constant drive to improve. I&apos;m eager to collaborate with
+          like-minded individuals and teams on challenging and exciting projects
+          within the ever-evolving field of web development.
           <br />
           {/* Feel free to explore my portfolio and get in touch with me if you&apos;re
           interested in working together or if you have any questions. Let&apos;s
