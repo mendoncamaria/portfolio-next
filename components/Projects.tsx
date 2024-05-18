021d8a87a680a968ef67b5316dcebd9db62eee85
Maria Mendonca <maria.mendonca156@gmail.com>
2024-05-18 14:40:43 +0530

    code optimization- project list

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,37 +1,54 @@
-import { FaProjectDiagram } from "react-icons/fa";
-import Title from "./Title";
-import ProjectCard from "./ProjectCard";
+import { FaProjectDiagram } from 'react-icons/fa';
+import Title from './Title';
+import ProjectCard from './ProjectCard';
 import img from '../public/next.svg';
 import weather from '../public/img/weather-img.png';
 import quiz from '../public/img/quiz-web.png';
 import gradient from '../public/img/gradient-web.png';
 import tempConv from '../public/img/temp-conv.png';
+// import todoList from '../public/img/TodoList.png';
+
+const ProjectList = [
+  {
+    img: weather,
+    title: 'Weather Application',
+    link: 'https://weather-mendoncamaria.vercel.app/',
+  },
+  {
+    img: quiz,
+    title: 'Quiz Website',
+    link: 'https://quiz-mendoncamaria.vercel.app/',
+  },
+  {
+    img: gradient,
+    title: 'Gradient Background Generator',
+    link: 'https://gradientbackground-mendoncamaria.netlify.app/',
+  },
+  {
+    img: tempConv,
+    title: 'Temperature Conversion',
+    link: 'https://temperatureconverter-mendoncamaria.netlify.app/',
+  },
+  // {
+  //   img: todoList,
+  //   title: 'Todo List',
+  //   link: 'https://mariamendonca-todo-app.netlify.app/',
+  // },
+];
 
 const Projects = () => {
   return (
     <div className="wrapper">
       <Title text="Projects" icon={<FaProjectDiagram />} />
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 place-items-center">
-        <ProjectCard
-          img={weather}
-          title="Weather Application"
-          link="https://weather-mendoncamaria.vercel.app/"
-        />
-        <ProjectCard
-          img={quiz}
-          title="Quiz Website"
-          link="https://quiz-mendoncamaria.vercel.app/"
-        />
-        <ProjectCard
-          img={gradient}
-          title="Gradient Background Generator"
-          link="https://gradientbackground-mendoncamaria.netlify.app/"
-        />
-        <ProjectCard
-          img={tempConv}
-          title="Project Name"
-          link="https://temperatureconverter-mendoncamaria.netlify.app/"
-        />
+        {ProjectList.map((item, index) => (
+          <ProjectCard
+            img={item.img}
+            title={item.title}
+            link={item.link}
+            key={index}
+          />
+        ))}
       </div>
     </div>
   );
