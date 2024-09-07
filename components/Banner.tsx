b810d238317f4d35c1c3ccba8f0347ac6d8e5fc0
Maria Mendonca <maria.mendonca156@gmail.com>
2024-09-07 10:33:16 +0530

    Updated Banner, removed photo from portfolio

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,25 +1,20 @@
-import Image from "next/image";
-import ProfileImg from "../public/img/profilepic.jpeg";
+import Image from 'next/image';
+import ProfileImg from '../public/img/profilepic.jpeg';
 
 const Banner = () => {
+  const getJobRole = () =>
+    `Full Stack Developer   |   Code Mentor   |   Continuous Learner`;
+
   return (
     <div className="wrapper flex flex-col md:flex-row items-center gap-10">
-      {/* Image */}
-      <div className="w-44 h-44 rounded-full bg-black">
-        <Image
-          className="w-full h-full rounded-full border-[1px] border-indigo-700 p-2 object-cover"
-          src={ProfileImg}
-          alt="ProfileImg"
-        />
-      </div>
-      {/* Description */}
-      <div className="w-3/4 flex flex-col items-center md:items-start    gap-2">
-        <h1 className="text-5xl font-bold text-white">Maria Mendonca</h1>
+      <div className="w-3/4 flex flex-col items-center md:items-start gap-4">
+        <h1 className="text-6xl  font-bold text-white">Maria Mendonca</h1>
         <h3 className="text-xl md:text-2xl font-semibold bg-clip-text text-transparent bg-gradient-to-r from-blue-300 via-red-300 to-indigo-300 tracking-wide">
-          Full Stack Developer
+        {getJobRole()}
         </h3>
+
         <p className="text-base tracking-wide text-center md:text-left">
-          {/* keep for any content */}
+          I design and code beautifully simple things, and I love what I do.
         </p>
       </div>
     </div>
