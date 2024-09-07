ff751a4896fe6b018c6dca4fb0c94d9addabc159
Maria Mendonca <maria.mendonca156@gmail.com>
2024-09-07 15:25:13 +0530

    updated paths

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,17 @@
-import Aboutme from "@/components/Aboutme";
 import Banner from "@/components/Banner";
 import Contact from "@/components/Contact";
-import Experience from "@/components/Experience";
+import Experience from "@/container/Experience";
 import Footer from "@/components/Footer";
-import Projects from "@/components/Projects";
-import Skills from "@/components/Skills";
+import Projects from "@/container/Projects";
+import Skills from "@/container/Skills";
+import AboutMe from "@/container/AboutMe";
 
 export default function Home() {
   return (
     <main className="w-full bg-gray-950 text-gray-300 px-4">
       <div className="max-w-screen-xl mx-auto lg:pt-10">
         <Banner />
-        <Aboutme />
+        <AboutMe />
         <Experience />
         <Skills />
         <Projects />
